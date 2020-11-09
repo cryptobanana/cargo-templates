145ae48f45bd6bb125dce1610e25fff296edb321
Dan Levin <dan@badpacket.in>
2020-11-09 21:43:22 +0100

    Introduce logging, test, and additional Structopt examples

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
+use log;
+use log::LevelFilter::{Debug, Error, Info, Trace};
+use simple_logger::SimpleLogger;
+use std::path::PathBuf;
 use std::{
     fs,
     io::{self, Write},
 };
-
+use structopt::StructOpt;
 // (Buf) Uncomment these lines to have the output buffered, this can provide
 // better performance but is not always intuitive behaviour.
 // use std::io::BufWriter;
 
-use structopt::StructOpt;
-
 #[derive(StructOpt, Debug)]
 #[structopt(
     name = "{{project-name}}",
-    about = "",
+    about = "A short description of this project.",
     version = concat!(env!("CARGO_PKG_VERSION"), concat!("_", env!("GIT_SHORT_HASH")))
 )]
 struct Options {
-    // The pattern we want to look for.
+    /// Increase logging verbosity
+    #[structopt(short, parse(from_occurrences))]
+    verbosity: usize,
+
+    /// Example optional boolean flag
+    #[structopt(short, long)]
+    some_flag: Option<bool>,
+
+    /// Example filesystem path
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+
+    /// Example String-valued Argument
+    #[structopt(default_value = "some value", env = "SOME_ENV_VAR")]
     pattern: String,
-    // The path of the file we want to look at.
-    path: String,
 }
 
 fn main() {
     let args = Options::from_args();
+    let level = vec![Error, Info, Debug, Trace][(args.verbosity).min(3)];
+    if args.verbosity > 0 {
+        SimpleLogger::new()
+            .with_level(level)
+            .init()
+            .expect("SimpleLogger instantiation failed.");
+    }
+
+    log::info!("Logging with level {}", level);
+
     let contents = fs::read_to_string(&args.path).expect("Could not read file.");
     let mut stdout = io::stdout();
     // (Buf) Wraps stdout in a buffer.
@@ -31,7 +54,7 @@ fn main() {
 
     for (line_no, line) in contents.lines().enumerate() {
         if line.contains(&args.pattern) {
-            writeln!(stdout, "{}: {}", line_no + 1, line);
+            let _ = writeln!(stdout, "{}: {}", line_no + 1, line);
         }
     }
 }
